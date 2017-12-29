647e9547b9c57253783f98fb1c677da4feab510d
Daniel Nordmark <daniel.github@yodapp.se>
2017-12-29 11:33:29 +0100

    Fully working
    Added all coded

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,40 @@
 $(function() {
 
   // your code will go here
-  https://www.codeschool.com/users/2166554
+
+  var badges = document.getElementById('badges');
+  console.log(badges);
+
   $.ajax({
     url: 'https://www.codeschool.com/users/2166554.json',
     dataType: 'jsonp',
     success: function(response) {
       // handle response
-      console.log(response);
+      console.log('Downloaded user profile!');
+      response.courses.completed.forEach(function(data){
+        console.log(data);
+        var courseDiv = document.createElement('div');
+        courseDiv.className = 'course';
+
+        var courseTitleElement = document.createElement('h3');
+        var courseTitle = document.createTextNode(data.title);
+        courseTitleElement.appendChild(courseTitle);
+        courseDiv.appendChild(courseTitleElement);
+
+        var badgeElement = document.createElement('img');
+        badgeElement.setAttribute('src', data.badge);
+        courseDiv.appendChild(badgeElement);
+
+        var buttonElement = document.createElement('a');
+        buttonElement.setAttribute('href', data.url);
+        buttonElement.setAttribute('target', '_blank');
+        buttonElement.className = 'btn btn-primary';
+        var buttonElementText = document.createTextNode('See Course');
+        buttonElement.appendChild(buttonElementText);
+        courseDiv.appendChild(buttonElement);
+
+        badges.appendChild(courseDiv);
+      });
     }
   });
 
