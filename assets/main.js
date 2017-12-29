42f0215d4d7d37a092afcc0e091588607b2defec
Daniel Nordmark <daniel.github@yodapp.se>
2017-12-29 10:56:28 +0100

    Initial commit
    Only making the Ajax request

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
   // your code will go here
+  https://www.codeschool.com/users/2166554
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2166554.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      console.log(response);
+    }
+  });
 
 });
